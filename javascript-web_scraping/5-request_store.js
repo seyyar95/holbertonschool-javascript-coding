501a9d6a94d99d39d5987e54cb91fda55d32510d
seyyar95 <heydrovsyyar@gmail.com>
2024-05-15 19:15:42 +0400

    muspiroL

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const fs = require('fs');
+
+request(`${process.argv[2]}`, (err, res, body) => {
+  if (err) throw err;
+  fs.writeFile(`${process.argv[3]}`, (body), 'utf8', (err) => {
+    if (err) throw err;
+  });
+});
