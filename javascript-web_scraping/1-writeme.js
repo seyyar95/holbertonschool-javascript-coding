48fcf3175a5a87a780a3e134c2d85192ecd500c3
seyyar95 <heydrovsyyar@gmail.com>
2024-05-15 14:00:39 +0400

    roses are read

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+let data = process.argv[3]
+
+fs.writeFile(`${process.argv[2]}`, data, 'utf8', (err) => {
+  if (err) {
+    console.log(err)
+  }
+})
