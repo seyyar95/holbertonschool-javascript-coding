d6e0f30433ec0294da776f2ef9ad12eedfdbebf4
seyyar95 <heydrovsyyar@gmail.com>
2024-05-15 15:18:46 +0400

    error handling

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,6 @@
 const request = require('request');
 
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
+  if (err) throw err;
   console.log(JSON.parse(body).title);
 });
