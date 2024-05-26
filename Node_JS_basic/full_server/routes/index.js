1d0dde0f36c878486f006c57b46172d60493e780
Səyyar Heydərov <heydrovsyyar@gmail.com>
2024-05-26 21:38:54 +0400

    index.js

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,7 +1,5 @@
-/* eslint-disable */
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
-
 
 const mapRoutes = (app) => {
   app.get('/', AppController.getHomepage);
